ca3e595e30f0dacb0ee5820e93f2a151a7595db4
Dan <danparise@outlook.com>
2020-06-16 13:01:09 +1000

    update

--- Gui_redesign/View/welcome_page.py
+++ Gui_redesign/View/welcome_page.py
@@ -3,6 +3,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
+        MainWindow.setWindowTitle("OnkoDICOM")
         MainWindow.setWindowModality(QtCore.Qt.NonModal)
         MainWindow.setEnabled(True)
         MainWindow.setFixedSize(939, 594)
@@ -31,6 +32,7 @@ class Ui_MainWindow(object):
         self.logo.setScaledContents(True)
         self.logo.setObjectName("logo")
 
+        # Frame Layout
         self.gridLayout_2.addWidget(self.logo, 4, 0, 1, 1)
         self.frame = QtWidgets.QFrame(self.centralwidget)
         self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
@@ -38,21 +40,25 @@ class Ui_MainWindow(object):
         self.frame.setObjectName("frame")
         self.gridLayout = QtWidgets.QGridLayout(self.frame)
         self.gridLayout.setObjectName("gridLayout")
-        self.continueButton = QtWidgets.QPushButton(self.frame)
+        self.openButton = QtWidgets.QPushButton(self.frame)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.continueButton.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(self.openButton.sizePolicy().hasHeightForWidth())
 
-        self.continueButton.setSizePolicy(sizePolicy)
-        self.continueButton.setFocusPolicy(QtCore.Qt.StrongFocus)
-        self.continueButton.setLayoutDirection(QtCore.Qt.LeftToRight)
-        self.continueButton.setAutoDefault(True)
-        self.continueButton.setDefault(False)
-        self.continueButton.setFlat(False)
-        self.continueButton.setObjectName("continueButton")
-        self.continueButton.setStyleSheet("background-color: #9370DB;" "border-width: 8px;" "border-radius: 20px;" "padding: 6px;" "color:white;") # Self added
-        self.gridLayout.addWidget(self.continueButton, 2, 0, 1, 1, QtCore.Qt.AlignHCenter) # Keeps button in middle
+        # Button
+        self.openButton.setSizePolicy(sizePolicy)
+        self.openButton.setFocusPolicy(QtCore.Qt.StrongFocus)
+        self.openButton.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.openButton.setAutoDefault(True)
+        self.openButton.setDefault(False)
+        self.openButton.setFlat(False)
+        self.openButton.setObjectName("continueButton")
+        self.openButton.setText("Open Patient")
+        self.openButton.setStyleSheet("background-color: #9370DB;" "border-width: 8px;" "border-radius: 20px;" "padding: 6px;" "color:white;") # Self added
+        self.gridLayout.addWidget(self.openButton, 2, 0, 1, 1, QtCore.Qt.AlignHCenter) # Keeps button in middle
+
+        self.openButton.clicked.connect(self.buttonClicked) # signal opening of Patient
 
         self.frame_2 = QtWidgets.QFrame(self.frame)
         self.frame_2.setFrameShape(QtWidgets.QFrame.StyledPanel)
@@ -66,11 +72,12 @@ class Ui_MainWindow(object):
         self.label.setScaledContents(True)
         self.label.setObjectName("label")
 
-        self.welcome = QtWidgets.QLabel(self.frame_2)
-        self.welcome.setGeometry(QtCore.QRect(200, 350, 508, 106))
-        self.welcome.setTextFormat(QtCore.Qt.AutoText)
-        self.welcome.setScaledContents(False)
-        self.welcome.setObjectName("welcome")
+        self.welcomeTxt = QtWidgets.QLabel(self.frame_2)
+        self.welcomeTxt.setGeometry(QtCore.QRect(200, 350, 508, 106))
+        self.welcomeTxt.setTextFormat(QtCore.Qt.AutoText)
+        self.welcomeTxt.setText("<html><head/><body><p align=\"center\"><span style=\" font-size:26pt; font-weight:600;\">Welcome to OnkoDICOM!</span></p><p align=\"center\"><br/></p><p align=\"center\"><span style=\" font-size:9pt;\">OnkoDICOM - the solution for producing data for analysis from your oncology plans and scans</span></p></body></html>")
+        self.welcomeTxt.setScaledContents(False)
+        self.welcomeTxt.setObjectName("welcome")
 
         self.gridLayout.addWidget(self.frame_2, 0, 0, 1, 1)
         self.gridLayout_2.addWidget(self.frame, 5, 0, 1, 1)
@@ -81,16 +88,10 @@ class Ui_MainWindow(object):
         self.statusbar.setSizeGripEnabled(False) # Remove expanding window option
         MainWindow.setStatusBar(self.statusbar)
 
-        self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "OnkoDICOM"))
-        self.continueButton.setText(_translate("MainWindow", "  Open Patient  "))
-        self.welcome.setText(_translate("MainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-size:26pt; font-weight:600;\">Welcome to OnkoDICOM!</span></p><p align=\"center\"><br/></p><p align=\"center\"><span style=\" font-size:9pt;\">OnkoDICOM - the solution for producing data for analysis from your oncology plans and scans</span></p></body></html>"))
-
-
+    def buttonClicked(self):
+        print("Button has been pressed")
 
 if __name__ == "__main__":
     import sys
