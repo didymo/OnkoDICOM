f94904bf661d3d670772ecae1009082103ed8937
fincap <finnkerman@gmail.com>
2020-06-28 19:42:09 +1000

    Temporary connection between the new boot window and existing patient window created.
    This currently means the window will hang for a minute or two while the patient window loads.
    This is soon to be replaced by a progress bar to provide feedback to the user.
    Multiprocessing for DVH calculation will also assist in speeding up this process.

--- src/View/open_patient.py
+++ src/View/open_patient.py
@@ -1,9 +1,13 @@
+import os
+
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import QCoreApplication, QThreadPool
 from PyQt5.QtWidgets import QTreeWidgetItem
 from PyQt5.Qt import Qt
+from pydicom import dcmread
 
-from src.Model import DICOMDirectorySearch
+from src.Controller import interPageController
+from src.Model import DICOMDirectorySearch, ImageLoading
 from src.Model.Worker import Worker
 
 
@@ -130,7 +134,26 @@ class UIOpenPatientWindow(object):
         selected_files = []
         for item in self.get_checked_leaves():
             selected_files += item.dicom_object.get_files()
-        print(selected_files)  # This needs to be replaced with a link to the existing OnkoDICOM main page
+
+        # TODO the following code should be run in a separate thread/process inside the Model directory.
+        path = os.path.dirname(os.path.commonprefix(selected_files))  # Temporary patch, gets the common root folder.
+        read_data_dict, file_names_dict = ImageLoading.get_datasets(selected_files)
+        dataset_rtss = dcmread(file_names_dict['rtss'])
+        dataset_rtdose = dcmread(file_names_dict['rtdose'])
+
+        rois = ImageLoading.get_roi_info(dataset_rtss)
+        raw_dvh = ImageLoading.calc_dvhs(dataset_rtss, dataset_rtdose, rois)
+        dvh_x_y = ImageLoading.converge_to_0_dvh(raw_dvh)
+        dict_raw_contour_data, dict_numpoints = ImageLoading.get_raw_contour_data(dataset_rtss)
+        dict_pixluts = ImageLoading.get_pixluts(read_data_dict)
+
+        # TODO the following code should be executed from a Controller class, not a View class.
+        # Loads the main window  by providing the necessary data obtained by the progress bar
+        patient_window = interPageController.MainWindow(path, read_data_dict, file_names_dict, rois, raw_dvh,
+                                         dvh_x_y, dict_raw_contour_data, dict_numpoints, dict_pixluts)
+        patient_window.show()
+        self.close()
+
 
     def get_checked_leaves(self):
         """
