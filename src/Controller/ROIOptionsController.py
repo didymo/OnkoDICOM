0009fe9a6eb02e4d4feda794b3e111255e96c17c
had647 <had647@uowmail.edu.au>
2020-09-04 03:39:50 +1000

    Rework ROI Delete Window to make the GUI consistent and reponsive

--- src/Controller/ROIOptionsController.py
+++ src/Controller/ROIOptionsController.py
@@ -1,39 +1,16 @@
-######################################################################################################
-#                                                                                                    #
-#   This file handles all the processes done within the ROI Delete and Draw button                   #
-#                                                                                                    #
-######################################################################################################
-import csv
-import sys
-import webbrowser
-from PyQt5.QtWidgets import QFileDialog, QMessageBox
 from src.View.mainpage.DeleteROIWindow import *
 from src.Controller.AddOnOptionsController import *
 from pydicom import Dataset
 
-######################################################################################################
-#                                                                                                    #
-#   Create the ROI Delete Options class based on the UI from the file in View/ROI Delete Optio       #
-#                                                                                                    #
-######################################################################################################
-
-
-class RoiDeleteOptions(QtWidgets.QMainWindow, Ui_DeleteROIWindow):
-    structure_delete = QtCore.pyqtSignal(tuple)  # new PyDicom dataset
-
+# Create the ROI Delete Options class based on the UI from the file in View/ROI Delete Option
+class RoiDeleteOptions(QtWidgets.QMainWindow, UIDeleteROIWindow):
+    deleting_rois_structure_tuple = QtCore.pyqtSignal(tuple)  # new PyDicom dataset
     def __init__(self, window, rois, dataset_rtss):
         super(RoiDeleteOptions, self).__init__()
 
         self.window = window
-        self.setupUi(self, rois, dataset_rtss, self.structure_delete)
-
-###################################################################################################################
-#                                                                                                                 #
-# The class that will be called by the main page to access the ROI Options controller                             #
-#                                                                                                                 #
-###################################################################################################################
-
-
+        self.setup_ui(self, rois, dataset_rtss, self.deleting_rois_structure_tuple)
+# The class that will be called by the main page to access the ROI Options controller
 class ROIDelOption:
 
     def __init__(self, window):
@@ -42,5 +19,5 @@ class ROIDelOption:
 
     def show_roi_delete_options(self):
         self.options_window = RoiDeleteOptions(self.window, self.window.rois, self.window.dataset_rtss)
-        self.options_window.structure_delete.connect(self.window.structures_tab.structure_modified)
+        self.options_window.deleting_rois_structure_tuple.connect(self.window.structures_tab.structure_modified)
         self.options_window.show()
