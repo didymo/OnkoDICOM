2e351b312890e978c99d1406d9609482229c08e3
had647 <had647@uowmail.edu.au>
2020-09-04 11:28:21 +1000

    Minor Fix To Open Patient Window Drag And Drop, Added New DrawROI Window

--- src/Controller/ROIOptionsController.py
+++ src/Controller/ROIOptionsController.py
@@ -1,4 +1,5 @@
 from src.View.mainpage.DeleteROIWindow import *
+from src.View.mainpage.DrawROIWindow import *
 from src.Controller.AddOnOptionsController import *
 from pydicom import Dataset
 
@@ -21,3 +22,19 @@ class ROIDelOption:
         self.options_window = RoiDeleteOptions(self.window, self.window.rois, self.window.dataset_rtss)
         self.options_window.deleting_rois_structure_tuple.connect(self.window.structures_tab.structure_modified)
         self.options_window.show()
+
+# Create the ROI Draw Options class based on the UI from the file in View/ROI Draw Option
+class RoiDrawOptions(QtWidgets.QMainWindow, UIDrawROIWindow):
+    def __init__(self, window):
+        super(RoiDrawOptions, self).__init__()
+        self.window = window
+        self.setup_ui(self)
+# The class that will be called by the main page to access the ROI Options controller
+class ROIDrawOption:
+
+    def __init__(self, window):
+        super(ROIDrawOption, self).__init__()
+        self.window = window
+    def show_roi_draw_options(self):
+        self.draw_window = RoiDrawOptions(self.window)
+        self.draw_window.show()
