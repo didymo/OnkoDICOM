4c7317221c1d1f0fdfb39e4454dbf57fd4cd6786
fincap <finnkerman@gmail.com>
2020-09-11 17:14:28 +1000

    Created a rudimentary new patient window to test if memory leaks.

---
+++ src/View/mainwindowrestructure/NewMainPage.py
@@ -0,0 +1,26 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class UINewMainWindow:
+    pyradi_trigger = QtCore.pyqtSignal(str, dict, str)
+
+    def setup_ui(self, main_window_instance, patient_dict_container):
+        self.dataset = patient_dict_container.dataset
+        self.patient_name = self.dataset[0]['PatientName']
+        self.rtss_modified = False
+
+        main_window_instance.setFixedSize(800, 600)
+        main_window_instance.setWindowTitle("OnkoDICOM")
+        main_window_instance.setWindowIcon(QtGui.QIcon("src/Icon/DONE.png"))
+
+        self.central_widget = QtWidgets.QWidget()
+        self.central_widget_layout = QtWidgets.QVBoxLayout()
+
+        self.label_patient_name = QtWidgets.QLabel(str(self.patient_name))
+        self.button_open_patient = QtWidgets.QPushButton("Open new patient")
+
+        self.central_widget_layout.addWidget(self.label_patient_name)
+        self.central_widget_layout.addWidget(self.button_open_patient)
+
+        self.central_widget.setLayout(self.central_widget_layout)
+        main_window_instance.setCentralWidget(self.central_widget)
