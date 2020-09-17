0853966764ac47f9f4754d503302127c0a2a9f82
fincap <finnkerman@gmail.com>
2020-09-17 15:06:03 +1000

    Corrected error in Singleton definition.

--- src/Model/Singleton.py
+++ src/Model/Singleton.py
@@ -4,7 +4,7 @@
 class Singleton(type):
     _instances = {}
 
-    def __call__(self, cls, *args, **kwargs):
+    def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
