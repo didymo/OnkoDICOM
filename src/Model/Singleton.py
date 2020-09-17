2cd971df4c1674fedc44b9c9e87b10415b2ef08e
fincap <finnkerman@gmail.com>
2020-09-17 15:13:33 +1000

    Added explanation of how to use Singleton metaclass.

--- src/Model/Singleton.py
+++ src/Model/Singleton.py
@@ -8,3 +8,7 @@ class Singleton(type):
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
+
+# To use this class, create a child class with the keyword argument metaclass=Singleton. For example,
+# class PatientDictContainer(metaclass=Singleton):
+#     class definition
