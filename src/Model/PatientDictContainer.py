eb8ab01d2305632232a1c10dbfb7d8e998d9f4b6
fincap <finnkerman@gmail.com>
2020-09-22 12:46:54 +1000

    PatientDictContainer is now a Singleton.

--- src/Model/PatientDictContainer.py
+++ src/Model/PatientDictContainer.py
@@ -1,4 +1,9 @@
 """
+Base requirements for OnkoDICOM to run:
+    path
+    dataset
+    filepaths
+
 Keyword arguments for DICOM-RT:
     rois
     raw_dvh
@@ -7,21 +12,41 @@ Keyword arguments for DICOM-RT:
     num_points
     pixluts
 """
+from src.Model.Singleton import Singleton
 
 
-class PatientDictContainer:
+class PatientDictContainer(metaclass=Singleton):
 
-    def __init__(self, path, dataset, filepaths, **kwargs):
+    def __init__(self):
+        # Initialize base requirements
+        self.path = None                # The path of the loaded directory.
+        self.dataset = None             # Dictionary of PyDicom dataset objects.
+        self.filepaths = None           # Dictionary of filepaths.
+
+        self.additional_data = None     # Any additional values that are required (e.g. rois, raw_dvh, raw_contour, etc)
+
+    def set_base_values(self, path, dataset, filepaths, **kwargs):
         """
-        :param path: The path of the loaded directory.
-        :param dataset: Dictionary where keys are slice number/RT modality and values are PyDicom dataset objects.
-        :param filepaths: Dictionary where keys are slice number/RT modality and values are filepaths.
-        :param kwargs: Any additional values that are required (e.g. rois, raw_dvh, raw_contour, etc)
+        Used to initialize the data on the creation of a new patient.
+        :param path:
+        :param dataset:
+        :param filepaths:
+        :param kwargs:
+        :return:
         """
         self.path = path
         self.dataset = dataset
         self.filepaths = filepaths
         self.additional_data = kwargs
+
+    def clear(self):
+        """
+        Clears the data in order to prepare for a new patient to be opened.
+        """
+        self.path = None
+        self.dataset = None
+        self.filepaths = None
+        self.additional_data = None
 
     def add(self, key, value):
         """
