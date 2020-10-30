2a69934ee04431a1afbe8978f4dd59cf8cd8e50f
fincap <finnkerman@gmail.com>
2020-10-30 16:52:18 +1100

    [DRAW ROI] Added some comments describing what's going on.

--- src/View/mainpage/DrawROIWindow.py
+++ src/View/mainpage/DrawROIWindow.py
@@ -642,6 +642,8 @@ class UIDrawROIWindow:
     def on_save_clicked(self):
         # Make sure the user has clicked Draw first
         if self.ds is not None:
+            # Because the contour data needs to be a list of points that form a polygon, the list of pixel points need
+            # to be converted to
             hull = self.calculate_convex_hull_of_points()
             single_array = []
             for sublist in hull:
@@ -657,54 +659,6 @@ class UIDrawROIWindow:
         else:
             QMessageBox.about(self.draw_roi_window_instance, "Not Enough Data",
                               "Please ensure you have drawn your ROI first.")
-
-    # def alpha_shape(points, alpha, only_outer=True):
-    #     """
-    #     Compute the alpha shape (concave hull) of a set of points.
-    #     :param points: np.array of shape (n,2) points.
-    #     :param alpha: alpha value.
-    #     :param only_outer: boolean value to specify if we keep only the outer border
-    #     or also inner edges.
-    #     :return: set of (i,j) pairs representing edges of the alpha-shape. (i,j) are
-    #     the indices in the points array.
-    #     """
-    #     assert points.shape[0] > 3, "Need at least four points"
-    #
-    #     def add_edge(edges, i, j):
-    #         """
-    #         Add an edge between the i-th and j-th points,
-    #         if not in the list already
-    #         """
-    #         if (i, j) in edges or (j, i) in edges:
-    #             # already added
-    #             assert (j, i) in edges, "Can't go twice over same directed edge right?"
-    #             if only_outer:
-    #                 # if both neighboring triangles are in shape, it's not a boundary edge
-    #                 edges.remove((j, i))
-    #             return
-    #         edges.add((i, j))
-    #
-    #     tri = Delaunay(points)
-    #     edges = set()
-    #     # Loop over triangles:
-    #     # ia, ib, ic = indices of corner points of the triangle
-    #     for ia, ib, ic in tri.vertices:
-    #         pa = points[ia]
-    #         pb = points[ib]
-    #         pc = points[ic]
-    #         # Computing radius of triangle circumcircle
-    #         # www.mathalino.com/reviewer/derivation-of-formulas/derivation-of-formula-for-radius-of-circumcircle
-    #         a = np.sqrt((pa[0] - pb[0]) ** 2 + (pa[1] - pb[1]) ** 2)
-    #         b = np.sqrt((pb[0] - pc[0]) ** 2 + (pb[1] - pc[1]) ** 2)
-    #         c = np.sqrt((pc[0] - pa[0]) ** 2 + (pc[1] - pa[1]) ** 2)
-    #         s = (a + b + c) / 2.0
-    #         area = np.sqrt(s * (s - a) * (s - b) * (s - c))
-    #         circum_r = a * b * c / (4.0 * area)
-    #         if circum_r < alpha:
-    #             add_edge(edges, ia, ib)
-    #             add_edge(edges, ib, ic)
-    #             add_edge(edges, ic, ia)
-    #     return edges
 
     def calculate_convex_hull_of_points(self):
         z_of_slice = 0
