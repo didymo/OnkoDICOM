b51cb104ef0dcf744a2210db8a034133449ee08e
fincap <finnkerman@gmail.com>
2020-11-21 01:23:41 +1100

    Waiting for threads to execute is now done with qtbot rather than sleep.

--- test/test_model_worker.py
+++ test/test_model_worker.py
@@ -1,4 +1,3 @@
-from time import sleep
 from unittest.mock import Mock
 
 from PyQt5.QtCore import QThreadPool
@@ -6,39 +5,42 @@ from PyQt5.QtCore import QThreadPool
 from src.Model.Worker import Worker
 
 
-def test_worker_progress_callback():
+# qtbot is a pytest fixture used to test PyQt5. Part of the pytest-qt plugin.
+def test_worker_progress_callback(qtbot):
     func_to_test = Mock()
     w = Worker(func_to_test, "test", 3, progress_callback=True)
 
+    # This starts the Worker in the threadpool and then blocks the test from progressing until the finished signal is
+    # emitted. qtbot is a pytest fixture used to test PyQt5.
     threadpool = QThreadPool()
-    threadpool.start(w)
-    sleep(2)    # give necessary time for the new thread to spawn
+    with qtbot.waitSignal(w.signals.finished) as blocker:
+        threadpool.start(w)
 
     assert w.fn == func_to_test
     assert w.kwargs['progress_callback'] is not None
     func_to_test.assert_called_with("test", 3, progress_callback=w.kwargs['progress_callback'])
 
 
-def test_worker_progress_callback_false():
+def test_worker_progress_callback_false(qtbot):
     func_to_test = Mock()
     w = Worker(func_to_test, "test", 3, progress_callback=False)
 
     threadpool = QThreadPool()
-    threadpool.start(w)
-    sleep(2)    # give necessary time for the new thread to spawn
+    with qtbot.waitSignal(w.signals.finished) as blocker:
+        threadpool.start(w)
 
     assert w.fn == func_to_test
     assert 'progress_callback' not in w.kwargs
     func_to_test.assert_called_with("test", 3)
 
 
-def test_worker_no_progress_callback():
+def test_worker_no_progress_callback(qtbot):
     func_to_test = Mock()
     w = Worker(func_to_test, "test", 3)
 
     threadpool = QThreadPool()
-    threadpool.start(w)
-    sleep(2)    # give necessary time for the new thread to spawn
+    with qtbot.waitSignal(w.signals.finished) as blocker:
+        threadpool.start(w)
 
     assert w.fn == func_to_test
     assert 'progress_callback' not in w.kwargs
@@ -53,8 +55,6 @@ def test_worker_result_signal(qtbot):
     w.signals.result.connect(func_result)
 
     threadpool = QThreadPool()
-    # This starts the Worker in the threadpool and then blocks the test from progressing until the finished signal is
-    # emitted. qtbot is a pytest fixture used to test PyQt5.
     with qtbot.waitSignal(w.signals.finished) as blocker:
         threadpool.start(w)
 
