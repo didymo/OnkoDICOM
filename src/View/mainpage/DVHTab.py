36f04a49e5e66084a7d9882eb4c8e03833cad5a2
Lachlan <lachlanmcclymont25@gmail.com>
2021-07-23 20:11:41 +1000

    Applied fix to DVH tab

--- src/View/mainpage/DVHTab.py
+++ src/View/mainpage/DVHTab.py
@@ -133,14 +133,58 @@ class DVHTab(QtWidgets.QWidget):
         return fig
 
     def prompt_calc_dvh(self):
-        choice = QtWidgets.QMessageBox.question(self, "Calculate DVHs?", "Would you like to calculate DVHs? This may"
-                                                                         " take up to several minutes on some systems.",
-                                                QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No)
+        """ Windows displays buttons in a different order from Linux. A check for
+                    platform is performed to ensure consistency of button positioning across
+                    platforms.
+                """
+        if platform.system() == "Linux":
+            choice = QtWidgets.QMessageBox.question(self, "Calculate DVHs?", "RTSTRUCT and RTDOSE datasets identified. Would you "
+                                                                   "like to calculate DVHs? (This may take up to several "
+                                                                   "minutes on some systems.)",
+                                          QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No)
 
-        if choice == QtWidgets.QMessageBox.Yes:
-            progress_window = CalculateDVHProgressWindow(self, QtCore.Qt.WindowTitleHint | QtCore.Qt.WindowCloseButtonHint)
-            progress_window.signal_dvh_calculated.connect(self.dvh_calculation_finished)
-            progress_window.exec_()
+            if choice == QtWidgets.QMessageBox.Yes:
+                self.signal_advise_calc_dvh.emit(True)
+            else:
+                self.signal_advise_calc_dvh.emit(False)
+        else:
+            stylesheet_path = ""
+
+            # Select appropriate style sheet
+            if platform.system() == 'Darwin':
+                stylesheet_path = "res/stylesheet.qss"
+            else:
+                stylesheet_path = "res/stylesheet-win-linux.qss"
+
+            # Create a message box and add attributes
+            mb = QtWidgets.QMessageBox()
+            mb.setIcon(QtWidgets.QMessageBox.Question)
+            mb.setWindowTitle("Calculate DVHs?")
+            mb.setText("RTSTRUCT and RTDOSE datasets identified. Would you "
+                       "like to calculate DVHs? (This may take up to several "
+                       "minutes on some systems.)")
+            button_no = QtWidgets.QPushButton("No")
+            button_yes = QtWidgets.QPushButton("Yes")
+
+            """ We want the buttons 'No' & 'Yes' to be displayed in that exact order. QMessageBox displays buttons in
+                respect to their assigned roles. (0 first, then 0 and so on) 'AcceptRole' is 0 and 'RejectRole' is 1 
+                thus by counterintuitively assigning 'No' to 'AcceptRole' and 'Yes' to 'RejectRole' the buttons are 
+                positioned as desired.
+            """
+            mb.addButton(button_no, QtWidgets.QMessageBox.AcceptRole)
+            mb.addButton(button_yes, QtWidgets.QMessageBox.RejectRole)
+
+            # Apply stylesheet to the message box and add icon to the window
+            mb.setStyleSheet(open(stylesheet_path).read())
+            mb.setWindowIcon(QtGui.QIcon(resource_path("res/images/btn-icons/onkodicom_icon.png")))
+
+            mb.exec_()
+
+            if mb.clickedButton() == button_yes:
+                progress_window = CalculateDVHProgressWindow(self,
+                                                             QtCore.Qt.WindowTitleHint | QtCore.Qt.WindowCloseButtonHint)
+                progress_window.signal_dvh_calculated.connect(self.dvh_calculation_finished)
+                progress_window.exec_()
 
     def dvh_calculation_finished(self):
         # Clear the screen
