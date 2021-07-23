da0671c1a4e7e66fc0058344cbf78cde68577382
Joshua <jjs910@hotmail.com>
2021-07-23 12:05:25 +1000

    Add search for PET NAC or CTAC data

--- main.py
+++ main.py
@@ -1,55 +1,61 @@
+# NOTE:
+# This script assumes the presence of DICOM PET Image Storage files
+# in the folder /test/testdata/DICOM-PT-TEST. This data is currently not
+# provided due to privacy concerns.
+
+from pydicom import dcmread
+from pydicom.errors import InvalidDicomError
+
 import os
-import warnings
-import sys
 import platform
 
-from PySide6 import QtWidgets, QtCore
-from PySide6.QtGui import QFont
 
-from src.Model.Configuration import Configuration
-from src.Controller.TopLevelController import Controller
-warnings.filterwarnings("ignore")
+def find_DICOM_files(file_path):
+    """
+    Function to find directories of DICOM PET Image files in a given
+    folder.
+    :param file_path: File path of folder to search.
+    :return: Dictionary where key is PET Image type (NAC or CTAC) and
+             value is directory where PET Image files of that type are.
+    """
 
-QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
+    dicom_files = {}
 
-if __name__ == "__main__":
+    # Walk through directory
+    for root, dirs, files in os.walk(file_path, topdown=True):
+        for name in files:
+            # Attempt to open file as a DICOM file
+            try:
+                ds = dcmread(os.path.join(root, name))
+                if ds.SOPClassUID == "1.2.840.10008.5.1.4.1.1.128":
+                    if 'CorrectedImage' in ds:
+                        if "ATTN" in ds.CorrectedImage:
+                            dicom_files["PT CTAC"] = root
+                        else:
+                            dicom_files["PT NAC"] = root
+                    break
+            except (InvalidDicomError, FileNotFoundError):
+                pass
+    return dicom_files
 
-    # On some configurations error traceback is not being displayed
-    #     when the program crashes. This is a workaround.
-    sys._excepthook = sys.excepthook
 
-    def exception_hook(exctype, value, traceback):
-        print(exctype, value, traceback)
-        sys._excepthook(exctype, value, traceback)
-        sys.exit(1)
+if __name__ == '__main__':
+    # Load test DICOM files
+    if platform.system() == "Windows":
+        desired_path = "\\test\\testdata\\DICOM-PT-TEST"
+    elif platform.system() == "Linux" or platform.system() == "Darwin":
+        desired_path = "/test/testdata/DICOM-PT-TEST"
 
-    sys.excepthook = exception_hook
+    desired_path = os.path.dirname(os.path.realpath(__file__)) + desired_path
 
-    os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
-    app = QtWidgets.QApplication(sys.argv)
-
-    print("PDPI: " + str(app.primaryScreen().physicalDotsPerInch()))
-
-    # Set the font to Segoe UI, 9, when in windows OS
-    if platform.system() == 'Windows':
-        f = QFont("Segoe UI", 9)
-        app.setFont(f)
-    elif platform.system() == 'Darwin':
-        f = QFont("Helvetica Neue", 13)
-        app.setFont(f)
-
-    if len(sys.argv) > 1:
-        controller = Controller(default_directory=sys.argv[1])
-        controller.show_open_patient()
-    else:
-        # Connect to the SQLite database stored in the hidden directory
-        configuration = Configuration()
-        default_dir = configuration.get_default_directory()
-        if default_dir is None:
-            controller = Controller()
-            controller.show_first_time_welcome()
+    # list of DICOM test files
+    selected_files = find_DICOM_files(desired_path)
+    if "PT CTAC" in selected_files:
+        if "PT NAC" in selected_files:
+            print("CTAC and NAC data in DICOM Image Set")
         else:
-            controller = Controller(default_directory=default_dir)
-            controller.show_welcome()
-
-    sys.exit(app.exec_())
+            print("CTAC data in DICOM Image Set")
+    elif "PT NAC" in selected_files:
+        print("NAC data in DICOM Image Set")
+    else:
+        print("No PET data in DICOM Image Set")
