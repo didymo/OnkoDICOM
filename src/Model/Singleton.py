6940080cffcff20b81a9c7dd408abd2d203a284f
Josh <mail.joshthomas@gmail.com>
2021-07-30 19:38:30 +1000

    Updated src/Model files for PEP-8 compliance.

--- src/Model/Singleton.py
+++ src/Model/Singleton.py
@@ -1,4 +1,5 @@
-# Code credit to https://www.datacamp.com/community/tutorials/python-metaclasses
+# Code credit to:
+# https://www.datacamp.com/community/tutorials/python-metaclasses
 
 
 class Singleton(type):
@@ -6,9 +7,11 @@ class Singleton(type):
 
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+            cls._instances[cls] = \
+                super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
 
-# To use this class, create a child class with the keyword argument metaclass=Singleton. For example,
+# To use this class, create a child class with the keyword
+# argument metaclass=Singleton. For example,
 # class PatientDictContainer(metaclass=Singleton):
 #     class definition
