365bf360307902b69b843f6489292ae9dcc667a4
loc le <tienloc.le2604@gmail.com>
2021-08-09 15:57:08 +0700

    close application after tests

--- test/conftest.py
+++ test/conftest.py
@@ -2,11 +2,12 @@ import os
 import sqlite3
 from pathlib import Path
 import pytest
+from PySide6.QtWidgets import QApplication
 
 from src.Model.Configuration import Configuration
 
 
-@pytest.fixture(scope="module", autouse=True)
+@pytest.fixture(scope="session", autouse=True)
 def init_config(request):
     configuration = Configuration('TestConfig.db')
     db_file_path = Path(os.environ['USER_ONKODICOM_HIDDEN']).joinpath('TestConfig.db')
@@ -15,6 +16,7 @@ def init_config(request):
     configuration.update_default_directory(Path.cwd().joinpath('test', 'testdata'))
 
     def tear_down():
+        QApplication.quit()
         connection.close()
         if os.path.isfile(db_file_path):
             os.remove(db_file_path)
