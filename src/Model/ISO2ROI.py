d07ccb4f6889b3198658fd3aa55be8bfbc4cbb50
Joshua <jjs910@hotmail.com>
2021-08-17 12:30:52 +1000

    Add delete isodose ROI and recalculate

--- src/Model/ISO2ROI.py
+++ src/Model/ISO2ROI.py
@@ -201,9 +201,31 @@ class ISO2ROI:
             patient_dict_container.get("dataset_rtss").save_as(rtss_directory)
             patient_dict_container.set("rtss_modified", False)
 
-        # Calculate isodose ROI for each slice, skip if slice has no
-        # contour data
+        # Get existing ROIs
+        existing_rois = []
+        rois = patient_dict_container.get("dataset_rtss")
+        if rois:
+            for roi in rois.StructureSetROISequence:
+                existing_rois.append(roi.ROIName)
+
+        # Loop through each isodose level
         for item in contours:
+            # Delete ROI if it already exists to recreate it
+            if item in existing_rois:
+                dataset_rtss = ROI.delete_roi(dataset_rtss, item)
+
+                # Update patient dict container
+                current_rois = patient_dict_container.get("rois")
+                keys = []
+                for key, value in current_rois.items():
+                    if value["name"] == item:
+                        keys.append(key)
+                for key in keys:
+                    del current_rois[key]
+                patient_dict_container.set("rois", current_rois)
+
+            # Calculate isodose ROI for each slice, skip if slice has no
+            # contour data
             for i in range(slider_min, slider_max):
                 if not len(contours[item][i]):
                     continue
