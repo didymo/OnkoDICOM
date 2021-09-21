c87cffefa0f6ee885310db10aea0bef7ad681016
Joshua <jjs910@hotmail.com>
2021-09-21 13:20:41 +1000

    Add test for batch ROI name cleaning

--- test/test_model_batch_processes.py
+++ test/test_model_batch_processes.py
@@ -2,13 +2,16 @@ import os
 import pytest
 from pathlib import Path
 from pydicom import dcmread
+from pydicom.errors import InvalidDicomError
 from PySide6.QtWidgets import QApplication
 from src.Controller.BatchProcessingController import BatchProcessingController
 from src.Model import DICOMDirectorySearch
-from src.Model.batchprocessing.BatchProcessISO2ROI import BatchProcessISO2ROI
 from src.Model.batchprocessing.BatchProcessDVH2CSV import BatchProcessDVH2CSV
+from src.Model.batchprocessing.BatchProcessISO2ROI import BatchProcessISO2ROI
 from src.Model.batchprocessing.BatchProcessPyRad2CSV import \
     BatchProcessPyRadCSV
+from src.Model.batchprocessing.BatchProcessROINameCleaning import \
+    BatchProcessROINameCleaning
 
 
 class TestObject:
@@ -160,7 +163,62 @@ def test_batch_pyrad2csv(test_object):
         assert os.path.isfile(Path.joinpath(test_object.batch_dir, 'CSV',
                                             filename))
 
-# TODO ROI name cleaning
+
+def test_batch_roi_name_cleaning(test_object):
+    """
+    Test asserts an ROI changes name and one is deleted.
+    """
+    # Loop through patient datasets
+    for patient in test_object.get_patients():
+        cur_patient_files = BatchProcessingController.get_patient_files(
+            patient)
+
+        # Get RTSS file path, count number of ROIs
+        rtss_path = None
+        for root, dirs, files in os.walk(test_object.batch_dir, topdown=True):
+            for name in files:
+                try:
+                    ds = dcmread(os.path.join(root, name))
+                    if ds.SOPClassUID == '1.2.840.10008.5.1.4.1.1.481.3':
+                        rtss_path = os.path.join(root, name)
+                        break
+                except (InvalidDicomError, FileNotFoundError):
+                    pass
+
+        # Assert rtss exists
+        assert rtss_path is not None
+        assert os.path.exists(rtss_path)
+
+        ds = dcmread(rtss_path)
+        number_rois = len(ds.StructureSetROISequence)
+
+        # Create and setup the Batch Process
+        process = BatchProcessROINameCleaning(test_object.DummyProgressWindow,
+                                              test_object.DummyProgressWindow,
+                                              None)
+
+        # Set options (rename LUNGS to Lungs, delete ISO0760)
+        roi_options = {rtss_path: [['LUNGS', 1, 'Lungs'],
+                                   ['ISO0760', 2]]}
+        process.roi_options = roi_options
+
+        # Start the process
+        process.start()
+
+        # Assert the number of ROIs decreased by one
+        ds = dcmread(rtss_path)
+        new_number_rois = len(ds.StructureSetROISequence)
+        assert new_number_rois < number_rois
+
+        # Assert ROI name changed
+        for roi in ds.StructureSetROISequence:
+            if roi.ROIName == 'Lungs':
+                assert True
+                return
+
+        # Assert false if ROI name was not changed
+        assert False
+
 
 # TODO CSV 2 Clinical Data SR
 
