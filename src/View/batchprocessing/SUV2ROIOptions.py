269842e19a83875b21a73f635cab0e43f04165cf
Joshua <jjs910@hotmail.com>
2021-09-26 22:28:31 +1000

    Add batch SUV2ROI GUI

--- src/View/batchprocessing/SUV2ROIOptions.py
+++ src/View/batchprocessing/SUV2ROIOptions.py
@@ -1,5 +1,5 @@
 import platform
-from PySide6 import QtWidgets
+from PySide6 import QtGui, QtWidgets
 from src.Controller.PathHandler import resource_path
 
 
@@ -25,4 +25,19 @@ class SUV2ROIOptions(QtWidgets.QWidget):
             self.stylesheet_path = "res/stylesheet-win-linux.qss"
         self.stylesheet = open(resource_path(self.stylesheet_path)).read()
 
+        # Create QLabel to display information, and text and font
+        self.info_label = QtWidgets.QLabel()
+        self.text_font = QtGui.QFont()
+        text_info = "SUV2ROI can currently only be performed on PET images " \
+                    "that are stored in units of Bq/mL and that are decay " \
+                    "corrected. For best results, please ensure only one set " \
+                    "of compliant PET images is present in each dataset that " \
+                    "you wish to perform SUV2ROI on. For more information " \
+                    "on SUV2ROI functionality, see the User Manual."
+        self.text_font.setPointSize(12)
+        self.info_label.setText(text_info)
+        self.info_label.setFont(self.text_font)
+        self.info_label.setWordWrap(True)
+
+        self.main_layout.addWidget(self.info_label)
         self.setLayout(self.main_layout)
