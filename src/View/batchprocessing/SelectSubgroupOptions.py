9c7e215edc709bc669a8025cac4a18fb33e0b3a5
Sean Overton <sean14o8.so@gmail.com>
2022-09-06 22:34:38 +1000

    Introduced a QLabel for info alerts

--- src/View/batchprocessing/SelectSubgroupOptions.py
+++ src/View/batchprocessing/SelectSubgroupOptions.py
@@ -25,11 +25,17 @@ class SelectSubgroupOptions(QtWidgets.QWidget):
             self.stylesheet_path = "res/stylesheet-win-linux.qss"
         self.stylesheet = open(resource_path(self.stylesheet_path)).read()
 
-        # Directory text box
+        # Info messages
+        self.message = QtWidgets.QLabel(
+            "No Clinical-data-SR files located in current selected directory"
+            )
+
         # Filter options table
         self.filter_table = QtWidgets.QTableWidget(0, 0)
         self.filter_table.setStyleSheet(self.stylesheet)
 
+        # Set up layout
+        self.main_layout.addWidget(self.message)
         self.main_layout.addWidget(self.filter_table)
         self.setLayout(self.main_layout)
 
@@ -37,8 +43,22 @@ class SelectSubgroupOptions(QtWidgets.QWidget):
 
         self.filter_table.cellClicked.connect(self.select_filter_option_cell)
 
-    def show_filtering_options_in_table(self, options_data_dict):
+    def display_no_data(self):
+        print("display no data called")
+        self.message.setText("No Clinical-data-SR files located in current selected directory")
         self.filter_table.setRowCount(0)
+        self.filter_table.setColumnCount(0)
+        self.filter_table.setHorizontalHeaderLabels([])
+
+    def show_filtering_options_in_table(self, options_data_dict):
+        if not options_data_dict:
+            self.display_no_data()
+            return
+
+        self.message.setText("Select values to filter by:")
+        
+        self.filter_table.setRowCount(0)
+        self.filter_table.setColumnCount(0)
 
         options_data_dict.pop("HASHidentifier")
 
@@ -47,12 +67,12 @@ class SelectSubgroupOptions(QtWidgets.QWidget):
             self.filter_table.insertColumn(col)
             
             for row in range(0, len(options_data_dict[title])):
-                filter = QtWidgets.QTableWidgetItem(str(options_data_dict[title][row]))
+                filter_value = QtWidgets.QTableWidgetItem(str(options_data_dict[title][row]))
                 
                 if row >= self.filter_table.rowCount():
                     self.filter_table.insertRow(row)
 
-                self.filter_table.setItem(row, col, filter)
+                self.filter_table.setItem(row, col, filter_value)
 
         self.filter_table.setHorizontalHeaderLabels(options_data_dict.keys())
 
