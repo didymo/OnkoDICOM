57a7e0e48f50eb31338d93d781442d86c1af8637
Sean Overton <sean14o8.so@gmail.com>
2022-09-07 11:36:34 +1000

    Removed commented out code styling the batch process tabs on the left side of the window

--- src/View/BatchProcessingWindow.py
+++ src/View/BatchProcessingWindow.py
@@ -22,56 +22,11 @@ from src.View.batchprocessing.SUV2ROIOptions import SUV2ROIOptions
 from src.Model.BatchProcessFilter import BatchProcessFilterModel
 
 
-# class TabBar(QtWidgets.QTabBar):
-#     def tabSizeHint(self, index):
-#         s = QtWidgets.QTabBar.tabSizeHint(self, index)
-#         s.transpose()
-#         return s
-
-#     def paintEvent(self, event):
-#         painter = QtWidgets.QStylePainter(self)
-#         opt = QtWidgets.QStyleOptionTab()
-
-#         for i in range(self.count()):
-#             self.initStyleOption(opt, i)
-#             painter.drawControl(QtWidgets.QStyle.CE_TabBarTabShape, opt)
-#             painter.save()
-
-#             s = opt.rect.size()
-#             s.transpose()
-#             r = QtCore.QRect(QtCore.QPoint(), s)
-#             r.moveCenter(opt.rect.center())
-#             opt.rect = r
-
-#             c = self.tabRect(i).center()
-#             painter.translate(c)
-#             painter.rotate(90)
-#             painter.translate(-c)
-#             painter.drawControl(QtWidgets.QStyle.CE_TabBarTabLabel, opt)
-            # painter.restore()
-
-# class ProxyStyle(QtWidgets.QProxyStyle):
-#     def drawControl(self, element, opt, painter, widget):
-#         if element == QtWidgets.QStyle.CE_TabBarTabLabel:
-#             ic = self.pixelMetric(QtWidgets.QStyle.PM_TabBarIconSize)
-#             r = QtCore.QRect(opt.rect)
-#             w = opt.rect.width() + self.pixelMetric(QtWidgets.QStyle.PM_TabBarIconSize)
-#             r.setHeight(opt.fontMetrics.horizontalAdvance("testin1234515512331233") + w)
-#             r.moveBottom(opt.rect.bottom())
-#             opt.rect = r
-#         QtWidgets.QProxyStyle.drawControl(self, element, opt, painter, widget)
-
 class CheckableTabWidget(QtWidgets.QTabWidget):
     """
     Creates a clickable tab widget.
     """
     checked_list = []
-
-    # def __init__(self, *args, **kwargs):
-    #     QtWidgets.QTabWidget.__init__(self, *args, **kwargs)
-    #     self.setTabBar(TabBar(self))
-    #     self.setTabPosition(QtWidgets.QTabWidget.West)
-        # self.setStyle(ProxyStyle())
 
     def addTab(self, widget, title):
         """
