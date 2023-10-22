379c815d5bedc2451bce7c89aca55e1466068ca1
Stuart Swerdloff <sjswerdloff@gmail.com>
2023-10-22 19:17:16 +1300

    bump version to 0.6.0-dev1

--- src/_version.py
+++ src/_version.py
@@ -1,7 +1,7 @@
 import re
 from typing import cast, Match
 
-__version__: str = '0.5.2'
+__version__: str = '0.6.0-dev1'
 
 result = cast(Match[str], re.match(r'(\d+\.\d+\.\d+).*', __version__))
 __version_info__ = tuple(result.group(1).split('.'))
