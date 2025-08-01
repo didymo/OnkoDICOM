5714b42181ed2ce8ce45c8dadffc982b13da5541
Kahre <MattCarlile590@gmail.com>
2025-08-01 14:33:02 +1000

    Added AutoSegmentationController.py and AutoSegmentationController class for AutoSegmentation feature

---
+++ src/Controller/AutoSegmentationController.py
@@ -0,0 +1,10 @@
+
+
+class AutoSegmentationController:
+    """
+    For the controlling of the UI elements in the View Class and the sending data to the Model class
+    As well as modifying data to communicate between the View and Model Classes
+    """
+
+    def __init__(self):
+        pass
