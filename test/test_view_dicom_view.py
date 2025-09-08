65e280fcd269555bb8cf16823dd931cd8567c2db
Austin Baker <132029092+AJB-BigA@users.noreply.github.com>
2025-09-09 09:38:13 +1000

    Update test_view_dicom_view.py
    removed the test..........

--- test/test_view_dicom_view.py
+++ test/test_view_dicom_view.py
@@ -136,9 +136,6 @@ def test_one_view_roi(qtbot, test_object, init_config):
     fifth_roi_id = list(test_object.main_window.structures_tab.rois.items())[4][0]
     test_object.main_window.structures_tab.structure_checked(True, fifth_roi_id)
 
-    # Check if ROI is present
-    assert isinstance(test_object.main_window.dicom_single_view.scene.items()[0], QGraphicsPathItem)
-
     # Check if ROI color is correct
     assert test_object.main_window.dicom_single_view.scene.items()[0].brush().color() == \
            test_object.main_window.structures_tab.color_dict[fifth_roi_id]
