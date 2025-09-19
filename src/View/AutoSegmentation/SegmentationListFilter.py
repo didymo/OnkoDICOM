e22b59ca1a50e131dfade045c91f33bafb5952f8
Kahre <MattCarlile590@gmail.com>
2025-09-19 18:48:17 +1000

    some refactoring to make the filtering lists in SegmentationListFilter.py to be more generic

--- src/View/AutoSegmentation/SegmentationListFilter.py
+++ src/View/AutoSegmentation/SegmentationListFilter.py
@@ -3,7 +3,7 @@ import pandas
 from src.Controller.PathHandler import resource_path
 
 
-def read_csv_to_pandas(csv: str, row_filter_column:str=None, row_filter_words: dict=None, column_list: list[str]=None) -> pandas.DataFrame:
+def read_csv_to_pandas(csv: str, row_filter_column:str=None, row_filter_words: dict | list[str]=None, column_list: list[str]=None) -> pandas.DataFrame:
     """
     Imports Segments list then filters it to the specified columns then return them as a dataFrame
 
@@ -13,34 +13,43 @@ def read_csv_to_pandas(csv: str, row_filter_column:str=None, row_filter_words: d
     :param column_list: list[str]: list of column names
     :return: pandas.DataFrame
     """
-    data = _read_csv_data_to_pandas(resource_path(csv))
-    data = _strip_whitespace(data)
+    data: pandas.DataFrame = _read_csv_data_to_pandas(resource_path(csv))
+    data: pandas.DataFrame = _strip_whitespace(data)
     data: pandas.DataFrame = _dict_row_filter(data, row_filter_column, row_filter_words)
     return _column_filter(data, column_list)
 
-def _dict_row_filter(data: pandas.DataFrame, csv_column: str = None, dict_obj: dict = None):
+def _dict_row_filter(data: pandas.DataFrame, csv_column: str = None, filter_values: dict | list[...] = None):
     """
     Method to read filter out irrelevant rows
 
     :param data: pandas.DataFrame
     :param csv_column: str: column name to filter rows to of the csv file
-    :param dict_obj: dict: dictionary with column names as keys and values as values
+    :param filter: dict | list[...] dictionary with column names as keys and values as values
     :return: pandas.DataFrame
     """
-    return data[data[csv_column].isin(dict_obj.values())]
 
-def _column_filter(data: pandas.DataFrame, column_list: list) -> pandas.DataFrame:
+    if filter_values is not None and csv_column is not None:
+        filter_list: dict | list[...] | None = None
+        if isinstance(filter_values, dict):
+            filter_list: list[str] = list(filter_values.values())
+        if isinstance(filter_values, list):
+            filter_list: list[str] = filter_values
+        if filter_list is not None:
+            return data[data[csv_column].isin(filter_list)]
+    return data
+
+def _column_filter(data: pandas.DataFrame, column_list: list[str]) -> pandas.DataFrame:
     """
     Method To filter Columns
     :param data: pandas.DataFrame
     :param *args: list[str]: list of column to filter to
     :return: pandas.DataFrame
     """
-    output = pandas.DataFrame()
+    output: pandas.DataFrame = pandas.DataFrame()
     if column_list is not None:
         for column in column_list:
             try:
-                output[column] = data[column]
+                output[column]: pandas.Series = data[column]
             finally:
                 pass
     return output
@@ -53,12 +62,13 @@ def _strip_whitespace(data: pandas.DataFrame) -> pandas.DataFrame:
     """
     for column in data.columns:
         if pandas.api.types.is_string_dtype(data[column]):
-            data[column] = data[column].str.strip()
+            data[column]: pandas.Series = data[column].str.strip()
     return data
 
-def _read_csv_data_to_pandas(csv: str):
+def _read_csv_data_to_pandas(csv: str) -> pandas.DataFrame:
     """
     Cached Method to return the row filtered pandas DataFrame
     :param csv: str: location of the csv file
+    :return: pandas.DataFrame
     """
     return pandas.read_csv(csv)
