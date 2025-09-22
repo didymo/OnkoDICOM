f3f796316b4f5df5b594739725cc3a9a4c75d370
mattc <mattcarlile590@gmail.com>
2025-09-22 11:38:06 +1000

    Removed Circular reference

--- src/Model/AutoSegmentation/AutoSegmentViewState.py
+++ src/Model/AutoSegmentation/AutoSegmentViewState.py
@@ -1,3 +1,4 @@
+from collections.abc import Callable
 from src.Model.Singleton import Singleton
 
 
@@ -7,8 +8,17 @@ class AutoSegmentViewState(metaclass=Singleton):
     To allow all data to be passed to view in a single object
     Allowing Construction in any object while having the same data.
     """
-    def __init__(self) -> None:
+    def __init__(self, callback: Callable[[], None]) -> None:
         """
         initialising members for struct class.
         """
-        self.segmentation_list = []
+        self.callback: Callable[[], None] = callback
+
+        self.segmentation_list: list[str] = []
+
+
+    def start_button_connection(self) -> None:
+        """
+        To communicate to controller the start button was clicked.
+        """
+        self.callback()
