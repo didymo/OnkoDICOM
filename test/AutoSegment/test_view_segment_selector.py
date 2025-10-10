b1b41cfdd5d7a9ad9eac5fe9762eb16d25afdfa0
Kahre <MattCarlile590@gmail.com>
2025-10-10 20:20:12 +1100

    SegmentSelectorWidget tests

--- test/AutoSegment/test_view_segment_selector.py
+++ test/AutoSegment/test_view_segment_selector.py
@@ -1,6 +1,6 @@
 import pytest
 import pathlib
-from PySide6.QtWidgets import QTreeWidgetItem
+from PySide6.QtWidgets import QTreeWidgetItem, QTreeWidget
 
 from src.View.AutoSegmentation.SegmentSelectorWidget import SegmentSelectorWidget
 
@@ -20,9 +20,16 @@ def tree_widget_item() -> QTreeWidgetItem:
     """
     To create a tree widget item with children to run tests on
 
-    :return: QTreeWidgetItem
+    :return: QTreeWidget
     """
-    pass
+    test_parent: QTreeWidgetItem = QTreeWidgetItem()
+    test_parent.setText(0, "parent")
+    test_child1: QTreeWidgetItem = QTreeWidgetItem(test_parent)
+    test_child1.setText(1,"child1")
+    test_child2: QTreeWidgetItem = QTreeWidgetItem(test_parent)
+    test_child2.setText(1,"child2")
+
+    return test_parent
 
 def test_object_created(selection_object: SegmentSelectorWidget) -> None:
     """
@@ -142,40 +149,16 @@ def test_get_csv_data(selection_object) -> None:
     assert not dataframe.shape[0] < 0
     assert not dataframe.shape[1] < 0
 
-def test_input_structure(selection_object) -> None:
-    pass
-
 def test_load_selections(selection_object) -> None:
+    """
+    Testing to see if multiple selections can be loaded into the selection tree
+
+    :return: None
+    """
     assert selection_object.get_segment_list() == []
     assert not selection_object.load_selections(["skull", "brain"])
     assert selection_object.get_segment_list() == ["skull", "brain"]
     assert not selection_object.get_segment_list() == []
-
-def test_resize_columns(selection_object) -> None:
-    """
-    Test to if resizing each of the columns int hte selection Tree works correctly
-
-    :return: None
-    """
-    assert selection_object._resize_columns(selection_object._selection_tree)
-
-def test_body_section_clicked(selection_object) -> None:
-    pass
-
-def test_parent_states(selection_object) -> None:
-    pass
-
-def test_child_states(selection_object) -> None:
-    pass
-
-def test_parent_section_changed(selection_object) -> None:
-    pass
-
-def test_specific_structure_changed(selection_object) -> None:
-    pass
-
-def test_setting_parent_states(selection_object) -> None:
-    pass
 
 def test_uniform_selection(selection_object) -> None:
     """
@@ -206,6 +189,46 @@ def test_deselect_all(selection_object):
     :return: None
     """
     assert not selection_object.deselect_all()
+
+def test_resize_columns(selection_object) -> None:
+    """
+    Test to if resizing each of the columns int hte selection Tree works correctly
+
+    :return: None
+    """
+    assert selection_object._resize_columns(selection_object._selection_tree)
+
+def test_input_structure(selection_object, tree_widget_item) -> None:
+    """
+    Testing is adding a Tree Widget item to a parent Tree Widget item works correctly
+
+    :param selection_object: SegmentSelectorWidget
+    :param tree_widget_item: QtWidgets.QTreeWidgetItem
+    :return: None
+    """
+    assert not tree_widget_item.child(2)
+    assert selection_object._input_structure(1, "child3", tree_widget_item)
+    assert tree_widget_item.child(2)
+    assert tree_widget_item.child(2).text(1).strip() == "child3"
+
+def test_body_section_clicked(selection_object, tree_widget_item) -> None:
+    pass
+
+def test_parent_states(selection_object, tree_widget_item) -> None:
+    pass
+
+def test_child_states(selection_object, tree_widget_item) -> None:
+    pass
+
+def test_parent_section_changed(selection_object, tree_widget_item) -> None:
+    pass
+
+def test_specific_structure_changed(selection_object, tree_widget_item) -> None:
+    pass
+
+def test_setting_parent_states(selection_object, tree_widget_item) -> None:
+    pass
+
 
 
 
